5d857addd6394a637cb433af60c99e9d29f9f845
Yanrui Jiao <yajiao@syr.edu>
2017-02-03 10:44:06 -0500

    Finished my homework

--- content/lessons/02/Now-You-Code/NYC4-Mad-Libs.py
+++ content/lessons/02/Now-You-Code/NYC4-Mad-Libs.py
@@ -9,5 +9,16 @@ http://www.madlibs.com and http://www.madtakes.com
 Your story should take at least 5 inputs, and should include more than
 once sentence. 
 '''
-
+x=input("Enter it here")
+y=input("Enter it here")
+z=input("Enter it here")
+a=input("Enter it here")
+b=input("Enter it here")
+c=input("Enter it here")
+print(x,"keep the secret, if you want to be", y)
+print("I have",x,"for you,but you have to keep the secret.")
+print("you can",z)
+print("where the wheather is",a)
+print ("A beautiful",b ,"will marry you, only if you keep the secret.")
+print("Guess what",c ,"will be given to you")
 
