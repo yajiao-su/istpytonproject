10cf8da9fdf2eeb6bd4c3cc812a2fa95514e10d1
Yanrui Jiao <yajiao@syr.edu>
2017-03-09 22:47:50 -0500

    completed

--- content/lessons/07/Now-You-Code/NYC3-Twitter-Hashtag.py
+++ content/lessons/07/Now-You-Code/NYC3-Twitter-Hashtag.py
@@ -30,5 +30,24 @@ you'll need to solve the problem!
 
 # TODO: Write Todo list then beneath write your code
 
-
+#input the sentence
+#if the sentence has no hashtages then print
+#if the sentence has hashtages then extract them
+#and need to find if hastages in not in the character, if it does, then it's wrong
 # Write code here
+tweet=input("Enter a tweet here:")
+each_word=tweet.split(" ")
+count=0
+if tweet.count("#")!=0:
+    for i in range(0,len(each_word)):
+        if(each_word[i].find('#')==0 and each_word[i].count("#")==1):
+            print("Hashtage:%s" %each_word[i])
+            count=count+1
+        elif(each_word[i].find('#')>=0) and (each_word[i].count("#")==1):
+            print("You have something wrong with the word %s\n A hashtage should always at the first place of a word." %each_word[i])
+        elif(each_word[i].count('#')>1):
+                  print("The word %s should not contain more than one hashtag!" %each_word[i])
+        else:
+                  continue
+else:
+    print("Hashtag: none")
--- content/lessons/07/Now-You-Code/NYC4-Sentiment-v1.py
+++ content/lessons/07/Now-You-Code/NYC4-Sentiment-v1.py
@@ -46,5 +46,36 @@ you'll need to solve the problem!
 '''
 
 # TODO: Write Todo list then beneath write your code
+#write function sentiment(input_text) score= -1 0,+1
+#use while loop
+#set input
 
 # Write code here
+def sentiment(positive_text,negative_text,input_text):
+    each_word=input_text.split(' ')
+    score = 0
+    for i in range(len(each_word)):
+        if each_word[i] in positive_text:
+            score=score+1
+        if each_word[i] in negative_text:
+            score=score-1
+    return score
+positive_text=['happy','glad','excited','extraordinary','like','love','cheer up']
+negative_text=['sad','angry','depressive','bad','down','hate']
+
+while True:
+    input_text=str(input("Enter your sentence here to detect the sentiment score"))
+    if input_text=="quit":
+        break
+    else:
+        score=sentiment(positive_text,negative_text,input_text)
+        if score==0:
+            state="neutral"
+        elif score>0:
+            state="positive"
+        elif score<0:
+            state="negative"
+        print("Your sentiment score is %d,and your state is %s" %(score,state))
+          
+              
+    
