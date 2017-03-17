0c45a660c465b358f9f5d042dea3c4ddbfaba923
Yanrui Jiao <yajiao@syr.edu>
2017-03-17 02:43:52 -0400

    completed

--- content/lessons/08/Now-You-Code/NYC3-Sentiment-v2.py
+++ content/lessons/08/Now-You-Code/NYC3-Sentiment-v2.py
@@ -23,5 +23,51 @@ negative words to the text files!
 '''
 
 # TODO: Write Todo list then beneath write your code
-
+#write function for test
+#connect files to the code
+#check if the word in the text
+#use the while loop to set input
 # Write code here
+def load_words(filename):
+    with open(filename,'r') as f:
+        return(f.read().split(' '))
+def sentiment(positive_text,negative_text,input_text):
+    each_word=input_text.split(' ')
+    list_symbol=(' ', '!', '@', '$', '%', '^', '&', '*', '()', '_', '-', '+', '=', '{}', '[]')
+    score=0
+    for i in range(len(each_word)):
+        if each_word[i].endswith(list_symbol)==True:
+            each_word[i]=each_word[i][0:(len(each_word[i])-1)]
+        if each_word[i] in positive_text:
+            score=score+1
+        if each_word[i] in negative_text:
+            score=score-1
+        elif each_word[i].startswith(list_symbol)==True:
+            each_word[i]=each_word[i][1:(len(each_word[i]))]
+            if each_word[i] in positive_text:
+                score=score+1
+            if each_word[i] in negative_text:
+                score=score-1
+        else:
+            if each_word[i] in positive_text:
+                score=score+1
+            if each_word[i] in negative_text:
+                score=score-1
+    return score
+positive_text=load_words("NYC3-pos.txt")
+negative_text=load_words("NYC3-neg.txt")
+while True:
+    input_text=str(input("Enter your sentence here to detect the sentiment score"))
+    if input_text=="quit":
+        break
+    else:
+        score=sentiment(positive_text,negative_text,input_text)
+        if score==0:
+            state="neutral"
+        elif score>0:
+            state="positive"
+        elif score<0:
+            state="negative"
+        print("Your sentiment score is %d,and your state is %s" %(score,state))
+          
+              
--- content/lessons/08/Now-You-Code/NYC4-Email-Harvest-Training.py
+++ content/lessons/08/Now-You-Code/NYC4-Email-Harvest-Training.py
@@ -24,6 +24,20 @@ you'll need to solve the problem!
 '''
 
 # TODO: Write Todo list then beneath write your code
-
+#connect the file to the code 
+#find each line in the code
+#count the number of emails it wrote to the file.
 # Write code here 
 
+readname="NYC4-mbox-short.txt"
+writename="NYC4-emails.txt"
+count=0
+with open(writename, 'w') as f_w:
+    f_w.write('')
+with open(readname, 'r') as f_r:
+    for line in f_r:
+        if line.startswith("From:")==True:
+            count=count+1
+            with open(writename,'a')as f_w:
+                f_w.write("%s\n" % line.replace('From: ','').strip())
+print("Wrote %d emails to %s" %(count,writename))
